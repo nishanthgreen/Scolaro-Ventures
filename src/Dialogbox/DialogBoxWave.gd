ec2dedb968e09aa30e884b245ce5bd26388d2e2a
nishanthgreen <nishanthgreen@gmail.com>
2021-03-04 17:29:06 +0530

    lvl3

---
+++ src/Dialogbox/DialogBoxWave.gd
@@ -0,0 +1,52 @@
+extends Control
+
+var dialog_lvl2 = [
+	'You have done good by comming all the way here',
+	'There is an enemy wave ahead',
+	'Kill all the enemies and you will get 1000points',
+	'GOOD LUCK.'
+	
+]
+
+var dialog_index = 0
+var finished = false
+
+func _ready():
+	if PlayerInfo.dialoglvl2 == 1:
+		return
+	
+	#set_physics_process(false)
+
+func _process(delta):
+	
+	if PlayerInfo.dialoglvl2 == 1:
+		queue_free()
+	$"Dialogbox/next-indicator".visible = finished
+	if Input.is_action_just_pressed("ui_accept"):
+		load_dialog()
+	
+	
+func load_dialog():
+	if dialog_index < dialog_lvl2.size():
+		finished = false
+		$Dialogbox/RichTextLabel.bbcode_text = dialog_lvl2[dialog_index]
+		$Dialogbox/RichTextLabel.percent_visible = 0
+		$Dialogbox/Tween.interpolate_property(
+			$Dialogbox/RichTextLabel, 'percent_visible', 0, 1, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+		)#for letter by letter animation ($node, property, start_value, end_value, time_taken)
+		$Dialogbox/Tween.start()
+	else:
+		PlayerInfo.dialoglvl2 = 1
+		queue_free()
+		get_tree().paused = false
+	dialog_index += 1
+
+
+func _on_Tween_tween_completed(object, key):
+	finished = true
+
+
+func _on_Left_area_body_entered(body):
+	$Dialogbox.visible = true
+	get_tree().paused = true
+	load_dialog()
--- src/Objects/Traps/Axe.gd
+++ src/Objects/Traps/Axe.gd
@@ -7,11 +7,11 @@ func _physics_process(delta):
 
 
 func _on_Killing_area_body_entered(body):
-	$axe_sound.play()
+	
 	if body.name == "Player_2":
 		emit_signal("kill")
 
 
 func _on_Killing_area_body_exited(body):
-	$axe_sound.stop()
+	
 	emit_signal("exited")
