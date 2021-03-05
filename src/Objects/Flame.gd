a3a01546dec07ad1c5290f1362928c91237acd2b
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:41:39 +0530

    Update Flame.gd

--- src/Objects/Flame.gd
+++ src/Objects/Flame.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
-const Speed = 250
+const Speed = 250      #speed at which flame is thrown by player
 var velocity = Vector2()
-var dir = 1
+var dir = 1            #variable to get the position of enemy (whether at right or left)
 
-
+#trash the flame as it exits from the screen 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
 
+#trash the flame as it enters the enemy's body
 func _on_Flame_body_entered(body: Node) -> void:
 	queue_free()
 
+#attacking using flame based on velocity and direction of enemy
 func _physics_process(delta: float) -> void:
 	velocity.x = Speed * delta * dir
 	translate(velocity)
@@ -21,8 +23,5 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.flip_h = false
-
-
-
 
 
