edf8bcfa46b20a2bf701298a84ee0c965bb9dc12
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:01:28 +0530

    Update Snake.gd

--- src/Objects/Traps/Snake.gd
+++ src/Objects/Traps/Snake.gd
@@ -1,19 +1,22 @@
 extends Area2D
 
-
+#snake animation being idle when the player enters the detection area
 func _on_Detection_area_body_entered(body):
 	if body.name == "player_2":
 		$snake.play("Appear")
 		if $snake.frame == 0:
 			$snake.play("Idle")
 
+#snake animation being idle when the player exit the detection area
 func _on_Detection_area_body_exited(body):
 	$snake.play("Idle")
 	
+#snake attacks as player enters the attacking area 
 func _on_Snake_body_attack_area_entered(body):
 	if body.name == "Player_2":
 		$snake.play("Attack")
 
+#trash the snake as attacking the player is completed
 func _on_snake_animation_finished() -> void:
 	if $snake.animation == "Attack":
 		queue_free()
@@ -23,7 +26,3 @@ func _ready():
 	if $snake.frame == 0:
 		$snake.stop()
 		
-
-
-
-
