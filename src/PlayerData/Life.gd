4086ecea550331efec3007bae8f0fb653729f357
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 23:00:36 +0530

    Update Life.gd

--- src/PlayerData/Life.gd
+++ src/PlayerData/Life.gd
@@ -1,12 +1,10 @@
 extends Control
 
+#update the health bar
 func _on_Player_2_health(health) -> void:
 	$HealthBar.value = health
 
+#update the health bar
 func _on_Player_2_heal(heal) -> void:
 	$HealthBar.value = heal
-
-
-
-
 
