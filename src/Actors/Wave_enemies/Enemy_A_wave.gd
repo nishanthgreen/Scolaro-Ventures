f76e785a96fc77ad952f983e3051efea4fc33419
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 21:18:32 +0530

    Update Enemy_A_wave.gd

--- src/Actors/Wave_enemies/Enemy_A_wave.gd
+++ src/Actors/Wave_enemies/Enemy_A_wave.gd
@@ -1,3 +1,5 @@
+#refer enemyy.gd
+
 extends "res://src/Actors/Actor.gd"
 signal enemy_A_died
 var score = 0
@@ -6,7 +8,6 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
-# *************************** detection of player's attack *********************
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	
 	$StompDetector/CollisionShape2D.disabled = true
@@ -16,24 +17,23 @@ func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	die()
 	
 	
-# *************************** enemy's movement *********************************
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	_velocity.x *= -1 if is_on_wall() else 1
 	
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-# *************************** HIT ANIMATION ************************************
+	
+
 	if _velocity.x != 0:
 		$enemy.animation = "attack"
 		$enemy.play()
 		$enemy.flip_h = _velocity.x > 0
-# *************************** MAKE ENEMY OUT FROM WORLD  ***********************
+
 	if $enemy.animation == "die":
 		if $enemy.frame == 6:
 			queue_free()
 			
 	
-# *************************** enemy's death ************************************
 func die() -> void:
 	PlayerInfo.wave_dead += 1
 	$enemy.animation = "die"
