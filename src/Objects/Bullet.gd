b368d36c5f9f3c969cff03e58c3eca4c07e27f92
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:12:29 +0530

    Update Bullet.gd

--- src/Objects/Bullet.gd
+++ src/Objects/Bullet.gd
@@ -1,23 +1,23 @@
 extends KinematicBody2D
 
-
+#variables
 var move = Vector2.ZERO
-var look_vec = Vector2.ZERO
+var look_vec = Vector2.ZERO      #variable to calculate the position of the player
 var target = null
 export var speed = 300
 
+#to get the current position of the moving player
 func _ready():
 	look_vec = target.position - global_position
 	
-	
-
+#to update the current position
 func _physics_process(delta):
 	move = Vector2.ZERO
 	move = move.move_toward(look_vec, delta)
 	move = move.normalized() * speed *delta
 	position += move
 
-
-
+#trash the bullet as it exits the screen
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
