8c8204369000f0e6455317eb238cf9f73640ee76
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:07:50 +0530

    Update BackButton.gd

--- src/Objects/BackButton.gd
+++ src/Objects/BackButton.gd
@@ -1,8 +1,6 @@
 extends Button
 
-
-#
-
-
+#scene changes to titlemenu as back button is pressed
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://src/levels/Title_screen/Title_menu.tscn")
+
