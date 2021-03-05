c4b1cd071ba6651d562ba5df00660055676bbaf4
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:31:34 +0530

    Update Fire.gd

--- src/Objects/Fire.gd
+++ src/Objects/Fire.gd
@@ -3,16 +3,17 @@ extends KinematicBody2D
 export (int) var Speed
 
 var velocity = Vector2()
-var dir = 1
+var dir = 1     #variable to get the player's position(right or left to enemy)
 
+#trash the fire as player attacks the enemy with flame
 func _on_flame__body_entered(body: Node) -> void:
 	queue_free()
 
-
+#trash the fire as it exits the screen
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
 
-
+#flipping of enemy's fire according to player's position
 func _physics_process(delta: float) -> void:
 	velocity.x = Speed * delta * dir
 	translate(velocity)
