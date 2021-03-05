3ffc877cbaebdcb46f4034c437394b51ab694614
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 21:46:09 +0530

    Update Axe.gd

--- src/Objects/Traps/Axe.gd
+++ src/Objects/Traps/Axe.gd
@@ -2,16 +2,17 @@ extends KinematicBody2D
 signal kill
 signal exited
 
+#playing of axe-movement animation as level begins
 func _physics_process(delta):
 	$AnimationPlayer.play("ping_pong")
 
-
+#detection of player entered and emiting a signal 'kill'
 func _on_Killing_area_body_entered(body):
 	
 	if body.name == "Player_2":
 		emit_signal("kill")
 
-
+#emiting of signal 'exited' as player exit from the axe
 func _on_Killing_area_body_exited(body):
 	
 	emit_signal("exited")
