23791eb9b3e47b9f3a0dcf4860ffe0768c140a24
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:46:31 +0530

    Update Spring.gd

--- src/Objects/Spring.gd
+++ src/Objects/Spring.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-var x: = 0
+var x: = 0      #to initialize the starting idle position of spring
 
 func _on_Spring_body_entered(body: Node) -> void:
-	
-	x = 1
+	x = 1             #to notify when the player is on the spring
 
 
+#spring movement based on position of player on/off the spring
 func _physics_process(delta: float) -> void:
 	if x == 1:
 		$AnimatedSprite.animation = "spring"
@@ -15,11 +15,3 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite.stop()
 	
 	
-
-	
-
-
-
-
-
-
