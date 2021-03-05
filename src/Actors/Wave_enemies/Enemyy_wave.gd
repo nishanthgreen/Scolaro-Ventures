9eb63053456458f31c378a4ab8a46791bf154e97
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 21:20:20 +0530

    Update Enemyy_wave.gd

--- src/Actors/Wave_enemies/Enemyy_wave.gd
+++ src/Actors/Wave_enemies/Enemyy_wave.gd
@@ -1,3 +1,5 @@
+#refer enemyy.gd
+
 extends "res://src/Actors/Actor.gd"
 
 signal enemy_died
@@ -8,7 +10,6 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 
-# *************************** detection of player's attack *********************
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	
 	$StompDetector/CollisionShape2D.disabled = true
@@ -17,36 +18,28 @@ func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	die()
 	
 
-# *************************** enemy's movement *********************************
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	_velocity.x *= -1 if is_on_wall() else 1
 	
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-# *************************** HIT ANIMATION ************************************
+	
+
 	if _velocity.x != 0:
 		$enemy.animation = "hit"
 		$enemy.play()
 		$enemy.flip_h = _velocity.x > 0
-# *************************** MAKE ENEMY OUT FROM WORLD  ***********************
+
 	if $enemy.animation == "die":
 		if $enemy.frame == 7:
 			queue_free()
 	"res://assets/sounds/pepSound4.ogg"
 
-# *************************** enemy's death ************************************
+
 func die() -> void:
 	PlayerInfo.wave_dead += 1
 	$enemy.animation = "die"
 	$soundsquash.play()
 	$enemy.play()
 	set_collision_mask_bit(0,false)
-
-
-
-
-
-
-
-
 
