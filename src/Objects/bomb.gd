316734201849d8e82a8ba44cf5c7b59ad926d506
Kamali Srinivasan <72341429+skamali1630@users.noreply.github.com>
2021-03-05 22:48:34 +0530

    Update bomb.gd

--- src/Objects/bomb.gd
+++ src/Objects/bomb.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
 
-
+#to play explosion animation as player enters the area of the bomb
 func _on_bomb_body_entered(body):
 	$explosion.play()
 	$bomb_sound.play()
  
 
-
-
+#trash the bomb as explosion gets over
 func _on_explosion_animation_finished() -> void:
-	#if $explosion.animation== "explosion":
 		queue_free()
+
